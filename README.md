d07d3befde29d50e2e9a5c5dacc8e8f5e813e010
Ezekiel Aquino <ezekielaquino@gmail.com>
2017-05-23 02:39:26 +0200

    🎉 Initial commit [new version]

--- README.md
+++ README.md
@@ -16,12 +16,9 @@ look of em. So this is me trying to update them for 2017.
 - Turn any element into a smooth-as-butter marquee
 - Text, images++<sup>*</sup> it'll do it
 - Style marquees as usual with CSS– get creative!
-- Set orientation, speed and direction
-- Have hundreds without any slowdown
-- Automatically adjusts if window is scaled up
-- Marquee only animates if it's in the viewport
-
-<sup>*</sup>we'll find out more
+- Set speed and direction
+- Have a ton without any slowdown
+- Responsive!
 
 ## Demo
 [MARQUEE3000 DEMO MARQUEE3000 DEMO MARQUEE3000 DEMO](https://ezekielaquino.github.io/Marquee3000/)
@@ -31,53 +28,37 @@ look of em. So this is me trying to update them for 2017.
 1. Include Marquee3000 in your html file. Download zip or install via `bower install marquee3000` || `npm install marquee3000`
 
     ```html
-        <script src="marquee3k.min.js"></script>
+        <script src="marquee3k.js"></script>
      ```
         
 2. Create an element with a `.marquee3k` class. You can pass different options such as speed, orientation and direction (optional). See below for options.
 
     ```html
         <div class="marquee3k" 
-            data-speed="60" → speed in pixels/sec (default: 50)
+            data-speed="0.25" → play around here
             data-reverse="bool" → default: R to L / T to B
-            data-vertical="bool" → [def] horizontal or [true] vertical
-            data-delay="2" → Initial pause before animating (in sec)
-            data-pausable="bool" → Pause marquee on hover
-            data-hover="bool" → Play marquee on hover
-            data-callback="functionName" → function called every cycle>
-            MARQUEE3000
+            data-pausable="bool" → Pause marquee on hover>
             <!--you can even have inline images,
             or any kind of html -->
-            <img src="trumpet.jpg" />
+            <h1>Some marquee content</h2>
         </div>
     ```
         
 3. Fill it up with text or images etc. (still finding out what you can do with it)
 
-4. In your js file or `<script>` just call `Marquee3k()` and you're all set!
+4. In your js file or `<script>` just call `Marquee3k.init()` and you're all set!
 
 ### Options
 
 You can set additional configuration options.
 
-    Marquee3k({
+    Marquee3k.init({
         selector: '.selector-name', // define a custom classname
-        randomSpeed: bool, // if true, each marquee will be assigned a random speed between 10-50px/sec
-        resizeDebounce: 500, // in ms, defaults to 500ms
-        spacing: 30 // number in px, space between copies - defaults to 30
+        spacing: '30px' // number + units (rem, etc), space between copies - defaults to 30
     });
 
-Marquee also adds a `is-ready` selector. You can use this to add and toggle entrance transitions, for example.
+Marquee also adds a `is-init` selector. You can use this to add and toggle entrance transitions, for example.
 
-### To Do
-- Make it play well with type plugins such as lettering.js or some other scripts
-- Some basic implementation of callbacks
-- More play options
-
-### Changes in 0.0.3 (Bear with me with the versioning)
-
-- Marquees can now take a onComplete/restart callback by passing `data-callback="functionName"`
-- `data-hover` only plays marquee on hover
 
 ### Questions
 
--- marquee3k.js
+++ marquee3k.js
@@ -1,314 +1,152 @@
 /**
  * MARQUEE 3000 MARQUEE 3000 MARQUEE 3000 MARQUEE 3000 MARQUEE 3000
  * http://github.com/ezekielaquino/marquee3000
- * Marquees for the new millenium
+ * Marquees for the new millenium v1.0
  * MIT License
  */
 
-(function() {
+;(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.Marquee3k = factory();
+  }
+}(this, function() {
   'use strict';
 
-  window.Marquee3k = function(options) {
-    options = options || {};
+  class Marquee3k {
+    constructor(element, options) {
+      this.element = element;
+      this.selector = options.selector;
+      this.speed = element.dataset.speed || 0.25;
+      this.pausable = element.dataset.pausable;
+      this.reverse = element.dataset.reverse;
+      this.paused = false;
+      this.parent = element.parentElement;
+      this.parentProps = this.parent.getBoundingClientRect();
+      this.content = element.children[0];
+      this.innerContent = this.content.innerHTML;
+      this.wrapStyles = '';
+      this.offset = 0;
 
-    var selector = options.selector || '.marquee3k';
-    var spacing = options.spacing || '30px';
-    var marquees = document.querySelectorAll(selector);
-    var winWidth = window.innerWidth;
+      this._setupWrapper();
+      this._setupContent();
+      this._setupEvents();
 
+      this.wrapper.appendChild(this.content);
+      this.element.appendChild(this.wrapper);
+    }
 
-    init();
+    _setupWrapper() {
+      this.parent.style.overflowX = 'hidden';
+      this.wrapper = document.createElement('div');
+      this.wrapper.classList.add('marquee3k__wrapper');
+      this.wrapStyles = 'white-space: nowrap;';
+      this.wrapper.setAttribute('style', this.wrapStyles);
+    }
 
+    _setupContent() {
+      this.content.classList.add(`${this.selector}__copy`);
+      this.content.style.display = 'inline-block';
+      this.contentWidth = this.content.offsetWidth;
 
-    /**
-     * INITIALIZE
-     */
-    function init() {
-      for (var i = 0; i < marquees.length; i++) {
-        var marquee = marquees[i];
-        var marqueeContent = marquee.innerHTML;
-        var marqueeContentWrap = document.createElement('div');
-        var marqueeElem = document.createElement('span');
-        var marqueeContentWrapWidth = 0;
-        var marqueeElemClone;
+      this.requiredReps = this.contentWidth > this.parentProps.width ? 2 : Math.ceil((this.parentProps.width - this.contentWidth) / this.contentWidth) + 1;
 
-        // Style the inner wrapper
-        marquee.innerHTML = '';
-        marquee.appendChild(marqueeContentWrap);
-        marqueeContentWrap = marquee.children[0];
-        marqueeContentWrap.style.whiteSpace = 'nowrap';
-        marqueeContentWrap.style.position = 'absolute';
+      for (let i = 0; i < this.requiredReps; i++) {
+        this._createClone();
+      }
 
-        // Set marquee properties
-        marquee.position = 0;
-        marquee.content = marqueeContent;
-        marquee.vertical = (marquee.dataset.vertical == 'true');
-        marquee.reverse = (marquee.dataset.reverse == 'true');
-        marquee.pausable = (marquee.dataset.pausable == 'true');
-        marquee.hover = (marquee.dataset.hover == 'true');
-        marquee.direction = marquee.reverse ? 1 : -1;
-        marquee.speed = (marquee.dataset.speed ? (marquee.dataset.speed / 60) : (50/60)) * marquee.direction;
-        marquee.delay = marquee.dataset.delay * 60 || 0;
-        marquee._delay = 0;
-        marquee.callback = marquee.dataset.callback;
+      if (this.reverse) {
+        this.offset = this.contentWidth * -1;
+      }
 
-        // Random speed
-        if (options.randomSpeed && !marquee.dataset.speed) {
-          var random = Math.floor(Math.random() * 50) + 10;
-          marquee.speed = (random / 60) * marquee.direction;
-        }
+      this.element.classList.add('is-init');
+    }
 
-        // Insert content and create a clone
-        marqueeElem.innerHTML = marquee.content;
-        marqueeElem.style.display = 'inline-block';
-        marqueeElem.style.paddingRight = spacing;
-        marqueeElemClone = marqueeElem.cloneNode(true);
+    _setupEvents() {
+      this.element.addEventListener('mouseenter', () => {
+        if (this.pausable) this.paused = true;
+      });
 
-        // Append first copy
-        marqueeContentWrap.appendChild(marqueeElem);
+      this.element.addEventListener('mouseleave', () => {
+        if (this.pausable) this.paused = false;
+      });
+    }
 
-        // Save initial width / height
-        marquee.contentWidth = marqueeContentWrap.offsetWidth;
-        marquee.contentHeight = getHeight(marquee);
+    _createClone() {
+      const clone = document.createElement('span');
+      clone.classList.add(`${this.selector}__copy`)
+      clone.innerHTML = this.innerContent;
+      this.wrapper.appendChild(clone);
+    }
 
-        // Set marquee elem style
-        marquee.style.overflow = 'hidden';
-        marquee.style.visibility = 'hidden';
-        marquee.style.position = 'relative';
-        marquee.style.width = (marquee.parentElement.offsetWidth || window.innerWidth) + 'px';
-        marquee.style.height = marquee.contentHeight + 'px';
-
-        // We make at least two copies (e.g. for LARGE elements, only needs two)
-        marqueeContentWrap.appendChild(marqueeElemClone);
-
-        // Sset initial wrap width
-        marqueeContentWrapWidth = marqueeContentWrap.offsetWidth;
-
-        // Set conditional styling (vertical / reversed)
-        if (marquee.vertical) {
-          marquee.classList.add('marquee3k--vertical');
-          marquee.style.width = marquee.contentHeight + 'px';
-          marquee.style.height = (marquee.parentElement.offsetHeight || window.innerHeight) + 'px';
-          marqueeContentWrap.style.transform = 'rotate(-90deg)';
-
-          if (!marquee.reverse) {
-            marqueeContentWrap.style.transformOrigin = '0% 0%';
-            marqueeContentWrap.style.left = 0;
-            marqueeContentWrap.style.top = '100%';
-          } else {
-            marqueeContentWrap.style.transformOrigin = '100% 100%';
-            marqueeContentWrap.style.bottom = '100%';
-            marqueeContentWrap.style.right = 0;
-          }
+    animate() {
+      if (!this.paused) {
+        if (!this.reverse) {
+          if (this.offset > this.contentWidth * -1) this.offset -= this.speed;
+          else this.offset = 0;
         } else {
-          marqueeContentWrap.style.top = 'calc(50% - ' + (marquee.contentHeight / 2) + 'px)';
-
-          if (marquee.reverse) {
-            marqueeContentWrap.style.right = 0;
-          }
+          if (this.offset < 0) this.offset += this.speed;
+          else this.offset = this.contentWidth * -1;
         }
 
-        // Multiply the contents to fit the container's width + allowance
-        while (marqueeContentWrapWidth < winWidth + (marquee.contentWidth * 2)) {
-          marqueeElemClone = marqueeElem.cloneNode(true);
-          marqueeContentWrap.appendChild(marqueeElemClone);
-          marqueeContentWrapWidth += marquee.contentWidth;
+        this.wrapper.setAttribute('style', `${this.wrapStyles} transform: translate3d(${this.offset}px, 0, 0);
+        `);
+      }
+    }
+
+    repopulate(difference, isLarger) {
+      this.contentWidth = this.content.offsetWidth;
+
+      if (isLarger) {
+        const amount = Math.ceil(difference / this.contentWidth) + 1;
+
+        for (let i = 0; i < amount; i++) {
+          this._createClone();
         }
+      }
+    }
 
-        marquee.isPaused = marquee.hover || !isInViewport(marquee);
-        marquee.style.visibility = 'visible';
-        marquee.classList.add('is-ready');
+    static init(options = { selector: 'marquee3k' }) {
+      const INSTANCES = [];
+      const marquees = document.querySelectorAll(`.${options.selector}`);
+      let previousWidth = window.innerWidth;
+      let timer;
 
-        // Add event listeners if marquee is pause/play on hover
-        (function(i) {
-          if (marquee.pausable) {
-            marquees[i].addEventListener('mouseenter', function() {
-              marquees[i].isPaused = true;
-            });
-
-            marquees[i].addEventListener('mouseleave', function() {
-              marquees[i].isPaused = false;
-            });
-          }
-
-          if (marquee.hover) {
-            marquees[i].addEventListener('mouseenter', function() {
-              marquees[i].isPaused = false;
-            });
-
-            marquees[i].addEventListener('mouseleave', function() {
-              marquees[i].isPaused = true;
-            });
-          }
-        })(i);
+      for (let i = 0; i < marquees.length; i++) {
+        const marquee = marquees[i];
+        const instance = new Marquee3k(marquee, options);
+        INSTANCES.push(instance);
       }
 
       animate();
 
-      // Resize - add more items if width < window width
-      // Scroll - Pause and play if marquee is in viewport
-      window.addEventListener('resize', onResize);
-      window.addEventListener('scroll', onScroll);
-    }
-
-
-    /**
-     * ANIMATE
-     */
-    function animate() {
-      for (var i = 0; i < marquees.length; i++) {
-        var marquee = marquees[i];
-        var callback = window[marquee.callback];
-
-        if (marquee._delay < marquee.delay) {
-          marquee._delay += 1;
-        } else if (!marquee.isPaused) {
-          if (!marquee.vertical) {
-            if (marquee.reverse) {
-              // on complete / restart
-              if (marquee.position >= marquee.contentWidth) {
-                if (marquee.callback && typeof callback === 'function') callback();
-                marquee.position = 0;
-              }
-            } else {
-              // on complete / restart
-              if (marquee.position <= marquee.contentWidth * -1) {
-                if (marquee.callback && typeof callback === 'function') callback();
-                marquee.position = 0;
-              }
-            }
-
-            marquee.position += marquee.speed;
-            marquee.children[0].style.transform = 'translate3d(' + marquee.position + 'px, 0, 0)';
-          } else {
-            if (marquee.reverse) {
-              if (marquee.position <= marquee.contentWidth * -1) marquee.position = 0;
-            } else {
-              if (marquee.position >= marquee.contentWidth) marquee.position = 0;
-            }
-
-            marquee.position -= marquee.speed;
-            marquee.children[0].style.transform = 'translate3d(0, ' + marquee.position + 'px, 0) rotate(-90deg)';
-          }
+      function animate() {
+        for (const instance of INSTANCES) {
+          instance.animate();
         }
+        window.requestAnimationFrame(animate);
       }
 
-      window.requestAnimationFrame(animate);
-    }
+      window.addEventListener('resize', () => {
+        clearTimeout(timer);
 
+        timer = setTimeout(() => {
+          const isLarger = previousWidth < window.innerWidth;
+          const difference = window.innerWidth - previousWidth;
 
-    /**
-     * RESIZE
-     */
-    function resize() {
-      for (var n = 0; n < marquees.length; n++) {
-        var marquee = marquees[n];
-        var marqueeContentWrap = marquee.children[0];
-
-        marquee.contentWidth = marqueeContentWrap.children[0].offsetWidth;
-        marquee.contentHeight = getHeight(marquee);
-        marquee.style.width = marquee.parentElement.offsetWidth || window.innerWidth + 'px';
-        marquee.style.height = marquee.contentHeight + 'px';
-        marqueeContentWrap.style.top = 'calc(50% - ' + (marquee.contentHeight / 2) + 'px)';
-      }
-    }
-
-
-    function onResize() {
-      debounce(function() {
-        resize();
-
-        if (window.innerWidth > winWidth) {
-          for (var i = 0; i < marquees.length; i++) {
-            var marquee = marquees[i];
-            var marqueeWrap = marquee.children[0];
-            var marqueeWrapWidth = marqueeWrap.offsetWidth;
-            var repeated = marquee.querySelector('span');
-
-            while (marqueeWrapWidth < window.innerWidth + (marquee.contentWidth * 2)) {
-              var clone = repeated.cloneNode(true);
-              marqueeWrap.appendChild(clone);
-              marqueeWrapWidth += repeated.offsetWidth;
-            }
+          for (const instance of INSTANCES) {
+            instance.repopulate(difference, isLarger);
           }
 
-          winWidth = window.innerWidth;
-        }
-      }, options.resizeDebounce || 500);
+          previousWidth = this.innerWidth;
+        });
+      }, 250);
     }
-
-
-    // get height of tallest element
-    function getHeight(marquee) {
-      var children = marquee.children[0].children;
-
-      children = Array.from(children);
-
-      children = children.map(function(elem) {
-        return elem.offsetHeight;
-      });
-
-      return Math.max(...children);
-    }
-
-
-    /**
-     * CHECK IF IN VIEWPORT
-     */
-    function onScroll() {
-      debounce(function() {
-        for (var i = 0; i < marquees.length; i++) {
-          if (isInViewport(marquees[i])) {
-            marquees[i].isPaused = false;
-          } else {
-            marquees[i].isPaused = true;
-          }
-        }
-      }, 500);
-    }
-
-
-    // MARQUEE 3000™
-  };
-
-
-  /**
-   * DEBOUNCE via David Walsh
-   */
-  function debounce(func, wait, immediate) {
-    var timeout;
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
   }
 
+  return Marquee3k;
 
-
-  /**
-   * WAYPOINT
-   */
-  function isInViewport(element) {
-    var rect = element.getBoundingClientRect();
-    var html = document.documentElement;
-
-    return (
-      !element.hover &&
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || html.clientHeight) &&
-      rect.right <= (window.innerWidth || html.clientWidth) || window.getComputedStyle(element, null).getPropertyValue('position') == 'fixed'
-    );
-  }
-
-
-
-
-
-})();
+}));
